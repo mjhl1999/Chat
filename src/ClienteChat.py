657f9773c50442b114549e57048737890c088f70
mjhl1999 <30506332+mjhl1999@users.noreply.github.com>
2018-08-25 17:01:33 -0500

    Primera versión: ServidorChat.py y ClienteChat.py

--- src/ClienteChat.py
+++ src/ClienteChat.py
@@ -1,1 +1,20 @@
-class CienteChat:
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+import sys
+
+def crea_socket(self):
+        socket = socket.socket()
+        return socket
+
+def conecta(self):
+    socket.connect( ('localhost', 8800) )
+    socket.send('Me he conectado al servidor')
+
+def recibe(self):
+    respuesta = socket.recv(1024)
+    print respuesta
+
+def desconecta(self):
+    socket.close()
--- src/ServidorChat.py
+++ src/ServidorChat.py
@@ -1,1 +1,25 @@
-class ServidorChat:
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+import sys
+
+def crea_socket(self):
+        socket = socket.socket()
+        socket.bind(('localhost',8800))
+        socket.listen(10)
+        return socket
+
+def conecta(self):
+    while True:
+        (conexion, direccion) = socket.accept()
+        print "Nueva conección establecida", direccion
+
+def recibe(self):
+    while True:
+        mensaje = self.crea_socket().recv(1024)
+        self.conecta().send("Te has conectado al servidor")
+        return mensaje
+
+def desconecta(self):
+    socket.close()
