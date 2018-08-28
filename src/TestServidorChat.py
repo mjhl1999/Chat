e4420461f4bef70e7c9ad091e5e2d26dfc04ba3b
Miren <mjhl1999@ciencias.unam.mx>
2018-08-28 13:25:36 -0500

    ...

--- src/TestServidorChat.py
+++ src/TestServidorChat.py
@@ -1,7 +1,11 @@
 import unittest
 
 class TestServidorChat(unittest.TestCase):
-    def test_envia(self):
+
+    def test_crea_socket(self):
+        pass
+
+    def test_conecta(self):
         servidor = ServidorChat()
         servidor.crea_socket()
         cliente = ClienteChat()
@@ -9,6 +13,18 @@ class TestServidorChat(unittest.TestCase):
         servidor.conecta()
         cliente.conecta()
         self.assertEquals(servidor.recibe(),cliente.recibe())
+
+    def test_recibe(self):
+        pass
+
+    def test_enviar(self):
+        pass
+
+    def test_desconecta(self):
+        pass
+
+    def test_envia(self):
+        pass
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestCliente)
