90f55f58098128ff976be8830f8c7630cf97552d
Miren <mjhl1999@ciencias.unam.mx>
2018-09-11 13:01:03 -0500

    más clases

--- src/Comportamiento.py
+++ src/Comportamiento.py
@@ -7,3 +7,49 @@ import sys
 import pickle
 
 class Comportamiento():
+
+    def conecta_cliente(self):
+        self.clientes.append(conexion)
+        print ("Nueva conección establecida con ", usuario)
+
+    def procesa_mensaje(self):
+        while True:
+            if len(clientes) > 0:
+                for c in clientes:
+                    try:
+                        mensaje = c.recv(1024)
+                        if data:
+                            self.enviar_a_todos(datos,c)
+                    except:
+                        pass
+
+    def enviar_a_todos(self):
+        for c in self.clientes:
+            try:
+                if c != cliente:
+    				c.send(msg)
+            except:
+                self.clientes.remove(c)
+
+    def recibe(self):
+        try:
+            self.mensaje = cliente.recv(1024)
+        except:
+            print("Error en el mensaje recibido")
+
+    def envia_especifico(self, mensaje, cliente):
+            if cliente in self.clientes:
+                try:
+        			c.send(msg)
+                except:
+                    print ("El usuario al que quieres enviar el mensjae no existe")
+
+    def desconecta(self):
+        mensaje = input('->')
+        if mensaje == 'salir':
+            for cliente in self.clientes:
+                cliente.close()
+            self.socket.close()
+            sys.exit()
+        else:
+            pass
--- src/ServidorChat.py
+++ src/ServidorChat.py
@@ -5,84 +5,38 @@ import socket
 import threading
 import sys
 import pickle
+from Comportamiento import Comportamiento
 
 class ServidorChat():
 
-    def __init__(self):
+    def __init__(self, host=sys.argv[1], port=sys.argv[2]):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind(('localhost', 8014))
+        self.socket.bind((host, port))
         self.socket.listen(10)
-        self.socket.setblocking(False)
-        self.clientes = []
+        clientes = []
 
-    def conectar(self):
+    def crea_socket(self):
         while True:
             try:
                 conexion, direccion = self.socket.accept()
-                self.clientes.append(conexion)
-                print "Nueva conección establecida", usuario
-                hilo_envia = threading.Thread(self.envia_mensaje()).start()
-                hilo_recibe = threading.Thread(self.recibe_mensaje()).start()
-                hilo_envia.join()
-                hilo_recibe.join()
+                hilo = threading.Thread(self.envia_mensaje()).start()
             except:
                 sys.exit()
-
-    def procesar(self):
-        while True:
-            if len(self.clientes) > 0:
-                for c in self.clientes:
-                    try:
-                        mensaje = c.recv(1024)
-                        if data:
-                            self.enviar_a_todos(datos,c)
-                    except:
-                        pass
-        return mensaje
-
-    def enviar(self):
-        for c in self.clientes:
-            try:
-                if c != cliente:
-    				c.send(msg)
-            except:
-                self.clientes.remove(c)
-
-    def recibe(self):
-        try:
-            self.usuario = cliente.recv(1024)
-            self.mensaje = cliente.recv(1024)
-        except:
-            print("Error en el mensaje recibido")
-
-    def envia_especifico(self, mensaje, cliente):
-        pass
-
-    def desconecta(self):
-        mensaje = input('->')
-        if mensaje == 'salir':
-            for cliente in self.clientes:
-                cliente.close()
-            self.socket.close()
-            sys.exit()
-        else:
-            pass
 
 
 def main():
     servidor = ServidorChat()
-    servidor.__init__()
     while True:
         try:
             conectar = threading.Thread(target=servidor.conectar()).start()
-            procesar = threading.Thread(target=servidor.procesar()).start()
+            Comportamiento.procesar = threading.Thread(target=servidor.procesar()).start()
 
             conectar.daemon = True
             conectar.start()
 
-            procesar.daemon = True
-            procesar.start()
+            Comportamiento.procesar.daemon = True
+            Comportamiento.procesar.start()
         except:
             print 'Algo salio mal'
             servidor.desconecta()
