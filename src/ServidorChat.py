7e58f40296ffa26cf71a5f8e29fa2b47590d7a68
Miren <mjhl1999@ciencias.unam.mx>
2018-09-14 14:55:35 -0500

    mejoras al servidor

--- src/ServidorChat.py
+++ src/ServidorChat.py
@@ -2,21 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import sys
 
 class ServidorChat(object):
 
-    host = 'localhost'
-    port = 8753
-    #crea nuevo socket
-    socket = socket.socket()
-    #establece conexión
-    socket.bind((host, port))
-    #establece cantidad de peticiones en cola que maneja el socket
-    socket.listen(10)
-    #lista que almacenara a los clientes
-    clientes = []
-    while True:
-        #acceptando peticiones
-        conexion, direccion = socket.accept()
-        print "Nueva conexion establecida"
-        conexion.send("Te has conectado al servidor")
+    def conecta(self, host, port):
+        #crea nuevo socket
+        self.socket = socket.socket()
+        #establece conexión
+        self.socket.bind( (host, port) )
+        #establece cantidad de peticiones en cola que maneja el socket
+        self.socket.listen(10)
+        #lista que almacenara a los clientes
+        clientes = []
+        while True:
+            #acceptando peticiones
+            conexion, direccion = self.socket.accept()
+            print "Nueva conexion establecida"
+            conexion.send("Te has conectado al servidor")
+
+        def desconecta():
+            
+
+def main():
+    host = str(sys.argv[1])
+    port = int(sys.argv[2])
+    servidor = ServidorChat()
+    servidor.conecta(host, port)
+
+main()
