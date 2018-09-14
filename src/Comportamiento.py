7b995b17fc413d063be8e8fcce8c423a6d7e7b23
Miren <mjhl1999@ciencias.unam.mx>
2018-09-14 15:38:46 -0500

    ...

--- src/Comportamiento.py
+++ src/Comportamiento.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import SalaDeChat
+
+
 class Comportamiento():
+
+    def ver_usuarios(self):
+
+    def envia_privado(self, usuario, mensaje):
+
+
+    def envia_publico(self, mensaje):
+
+        pass
+
+    def crea_sala(self, nombre):
+        pass
+
+    def invitar(self, sala, lista):
+        pass
+
+    def unirse(self, sala):
+        pass
+
+    def mensaje_sala(self, mensaje):
+        pass
+
+    def desconectar():
+        pass
