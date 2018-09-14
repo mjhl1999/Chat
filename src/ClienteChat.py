dbcf014e0a0d44f7e39a7c2e618556ddb49e7dc3
Miren <mjhl1999@ciencias.unam.mx>
2018-09-14 13:51:24 -0500

    Mejoras

--- src/ClienteChat.py
+++ src/ClienteChat.py
@@ -3,13 +3,41 @@
 
 import socket
 
-class ClienteChat():
+class ClienteChat(object):
 
-    #creando socket
-    socket = socket.socket()
-    #estableciendo conexion
-    socket.connect( ('localhost', 8754) )
-    socket.send("El cliente se a conectado")
-    respuesta = socket.recv(1024)
-    print respuesta
-    socket.close()
+    def __init__ (self, nombre, estado):
+        self.nombre = nombre
+        self.estado = estado
+
+    def magia(self):
+        #creando socket
+        self.my_socket = socket.socket()
+        #estableciendo conexion
+        self.my_socket.connect( ('localhost', 8754) )
+        self.my_socket.send("El cliente se a conectado")
+        respuesta = self.my_socket.recv(1024)
+        print respuesta
+        self.my_socket.close()
+
+
+def main():
+    print 'Ingresa tu nombre de usuario: '
+    usuario = raw_input()
+    print 'Elige tu estado (ACTIVE, AWAY, BUSY):'
+    estado = raw_input()
+    cliente = ClienteChat(usuario, estado)
+    cliente.magia()
+
+main()
+
+"""
+    try:
+        print 'Ingresa el host:'
+        host = raw_input()
+        print 'Ingresa el puerto:'
+        puerto = raw_input()
+        cliente.crea_socket()
+        cliente.conecta(host, puerto)
+    except:
+        print("Conexión rechazada, intente de nuevo,")
+"""
