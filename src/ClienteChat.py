8699dc2907ec88e37fb0ab162e7e159eac5f7ae0
Miren <mjhl1999@ciencias.unam.mx>
2018-09-14 13:03:25 -0500

    al menos el servidor y el cliente funcionan

--- src/ClienteChat.py
+++ src/ClienteChat.py
@@ -2,50 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import sys
-import threading
-import pickle
-import codecs
 
 class ClienteChat():
 
-    def __init__ (self, nombre, estado):
-        self.nombre = nombre
-        self.estado = estado
-
-    def crea_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
-        return socket
-
-    def conecta(self, host, puerto):
-        self.socket.connect( ('localhost', 8765) )
-        self.socket.send('El cliente', nombre, 'se ha conectado al servidor y esta', estado)
-
-
-def main():
-    try:
-        print 'Ingresa tu nombre de usuario: '
-        usuario = raw_input()
-        if (usuario == None or usuario == ""):
-            raise ValueError()
-        print 'Elige tu estado (ACTIVE, AWAY, BUSY):'
-        estado = raw_input()
-        if (estado != "ACTIVE" or estado != "AWAY" or estado != "BUSY"):
-            raise ValueError()
-        cliente = ClienteChat(self, usuario, estado)
-        print (usuario, ' Te has conectado al servidor')
-    except:
-         print("Usuario no valido, intenta de nuevo.")
-         sys.exit()
-"""
-    try:
-        print 'Ingresa el host:'
-        host = raw_input()
-        print 'Ingresa el puerto:'
-        puerto = raw_input()
-        cliente.crea_socket()
-        cliente.conecta(host, puerto)
-    except:
-        print("Conexión rechazada, intente de nuevo,")
-"""
-main()
+    #creando socket
+    socket = socket.socket()
+    #estableciendo conexion
+    socket.connect( ('localhost', 8754) )
+    socket.send("El cliente se a conectado")
+    respuesta = socket.recv(1024)
+    print respuesta
+    socket.close()
