e6155b5b0843dcf281fd827304d9c26cd71f2d09
Miren <mjhl1999@ciencias.unam.mx>
2018-09-15 22:28:50 -0500

    ahhhh

--- src/ClienteChat.py
+++ src/ClienteChat.py
@@ -3,45 +3,60 @@
 
 import socket
 import random
+import threading
+import pickle
+import sys
 
 class ClienteChat(object):
 
-    def __init__ (self, nombre, estado):
+    def __init__ (self, nombre, estado, host, port):
         self.nombre = nombre
         self.estado = estado
-
-    def conecta(self, host, port, tripleta):
         #creando socket
         self.socket = socket.socket()
         #estableciendo conexion
         self.socket.connect( (host, port) )
+        #recibe un mensaje desde el sevidor que nos dice si nos hemos conectado
         respuesta = self.socket.recv(1024)
         print (respuesta)
-        self.socket.send(str(tripleta))
+        #envia la informacion del cliente al servidor
+        #self.socket.send(str(tripleta))
         self.socket.close()
-
+        #hilo tipo daemon para el cliente
+        #mensaje = threading.Thread(target=self.recibe)
+        #mensaje.daemon = True
+        #mensaje.start()
+        #hilo principal y DISCONNECT
+        """
+        while True:
+            mensaje = raw_input('-> ')
+            if (mensaje != 'DISCONNECT'):
+                self.envia_publico(str(mensaje))
+            else:
+                self.socket.close()
+                sys.exit()
+        """
 def main():
     #try:
     print ('Ingresa tu nombre de usuario: ')
     usuario = raw_input()
     if (usuario == None or usuario == ""):
-            raise ValueError()
+        raise ValueError()
+        print 'Usuario Invalido'
     print ('Elige tu estado (ACTIVE, AWAY, BUSY):')
     estado = raw_input()
-    if (estado == 'ACTIVE' or estado == "AWAY" or estado == "BUSY"):
-        cliente = ClienteChat(usuario, estado)
-    else:
+    if (estado != 'ACTIVE' and estado != "AWAY" and estado != "BUSY"):
         raise ValueError()
+        print 'Estado Invalido'
     print 'Ingresa el host:'
     host = raw_input()
     print 'Ingresa el puerto:'
     port = input()
-    identificador = str(random.randint(1, 10000))
-    tripleta = (usuario, estado, identificador)
-    cliente.conecta(host, port, tripleta)
-"""
-    except:
-        print ('Algo salio mal, intente de nuevo')
-        sys.exit()
-"""
+    #identificador = str(random.randint(1, 10000))
+    #tripleta = (usuario, estado, identificador)
+    cliente = ClienteChat(usuario, estado, host, port)
+    #except:
+    #    print ('Algo salio mal, intente de nuevo')
+    #    sys.exit()
+
 main()
--- src/ServidorChat.py
+++ src/ServidorChat.py
@@ -3,35 +3,78 @@
 
 import socket
 import sys
+import threading
+import pickle
 
 class ServidorChat(object):
 
-    clientes = []
     def conecta(self, host, port):
+        #lista que almacenara a los clientes
+        self.clientes = []
         #crea nuevo socket
         self.socket = socket.socket()
-        #establece conexión
+        #establece conexión con el cliente
         self.socket.bind( (host, port) )
         #establece cantidad de peticiones en cola que maneja el socket
-        self.socket.listen(10)
-        #lista que almacenara a los clientes
+        self.socket.listen(10000)
+        #hilo tipo daemon para el servidor
+        aceptar = threading.Thread(target=self.aceptar)
+        aceptar.daemon = True
+        aceptar.start()
+        procesar = threading.Thread(target=self.procesar)
+        procesar.daemon = True
+        procesar.start()
+        print 'Teclee "DISCONNECT" para cerrar el servidor.'
         while True:
-            #acceptando peticiones
-            conexion, direccion = self.socket.accept()
-            print ("Nueva conexion establecida")
-            conexion.send("Te has conectado al servidor")
-            tripleta =  conexion.recv(1024)
-            self.clientes.append(str(tripleta))
-
-        def desconecta(self):
-            mensaje = input('->')
-            if mensaje == 'salir':
-                for cliente in self.clientes:
-                    cliente.close()
+            mensaje = raw_input('-> ')
+            if mensaje == 'DISCONNECT':
                 self.socket.close()
                 sys.exit()
             else:
                 pass
+
+
+    def aceptar(self):
+        #acceptando peticiones de clientes
+        while True:
+            conexion, direccion = self.socket.accept()
+            print ("Nueva conexion establecida")
+            conexion.send("Te has conectado al servidor")
+            #tripleta =  conexion.recv(1024)
+            #self.clientes.append(str(tripleta))
+            self.clientes.append(conexion)
+
+    def procesar(self):
+        while True:
+            if(len(self.clientes) > 0):
+                for i in self.clientes:
+                    try:
+                        mensaje = i.recv(1024)
+                        if mensaje:
+                            mensaje = str(mensaje)
+                            self.envia_publico(mensaje,i)
+                            print mensaje
+                    except:
+                        pass
+
+        def ver_usuarios(self):
+            peticion = conexion.recv(1024)
+            if (peticion == 'IDENTIFY'):
+                conexion.send(clientes)
+
+        def status(self, estado):
+            pass
+
+        def ussers(self):
+            pass
+
+        def envia_publico(self, mensaje):
+            for i in self.clientes:
+                try:
+                    if c != cliente: #evita que te autoenvies un mensaje
+                        c.send(pickle.dumps(mensaje))
+                except:
+                    self.clientes.remove(i)
 
 
 def main():
@@ -40,9 +83,8 @@ def main():
     port = int(sys.argv[2])
     servidor = ServidorChat()
     servidor.conecta(host, port)
-    """
-    except:
-        print ('Algo salio mal, intente de nuevo')
-        sys.exit()
-    """
+    #except:
+    print ('Algo salio mal, intente de nuevo')
+    sys.exit()
+
 main()
