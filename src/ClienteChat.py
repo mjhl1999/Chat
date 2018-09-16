7c663ce0501e052f0657db65ffc3e3f0c1bc3dcb
Miren <mjhl1999@ciencias.unam.mx>
2018-09-15 23:30:51 -0500

    El chat funciona

--- src/ClienteChat.py
+++ src/ClienteChat.py
@@ -10,32 +10,34 @@ import sys
 class ClienteChat(object):
 
     def __init__ (self, nombre, estado, host, port):
-        self.nombre = nombre
-        self.estado = estado
-        #creando socket
-        self.socket = socket.socket()
-        #estableciendo conexion
-        self.socket.connect( (host, port) )
-        #recibe un mensaje desde el sevidor que nos dice si nos hemos conectado
-        respuesta = self.socket.recv(1024)
-        print (respuesta)
-        #envia la informacion del cliente al servidor
-        #self.socket.send(str(tripleta))
-        self.socket.close()
-        #hilo tipo daemon para el cliente
-        #mensaje = threading.Thread(target=self.recibe)
-        #mensaje.daemon = True
-        #mensaje.start()
-        #hilo principal y DISCONNECT
-        """
-        while True:
-            mensaje = raw_input('-> ')
-            if (mensaje != 'DISCONNECT'):
-                self.envia_publico(str(mensaje))
-            else:
-                self.socket.close()
-                sys.exit()
-        """
+        	self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    		self.socket.connect((str(host), int(port)))
+
+    		recibe = threading.Thread(target=self.recibe)
+
+    		recibe.daemon = True
+    		recibe.start()
+
+    		while True:
+    			mensaje = raw_input('->')
+    			if mensaje != "DISCONNECT":
+    				self.envia(mensaje)
+    			else:
+    				self.socket.close()
+    				sys.exit()
+    def recibe(self):
+		while True:
+			try:
+				data = self.socket.recv(1024)
+				if data:
+					print(pickle.loads(data))
+			except:
+				pass
+
+    def envia(self, mensaje):
+		self.socket.send(pickle.dumps(mensaje))
+
+
 def main():
     #try:
     print ('Ingresa tu nombre de usuario: ')
@@ -52,11 +54,9 @@ def main():
     host = raw_input()
     print 'Ingresa el puerto:'
     port = input()
-    #identificador = str(random.randint(1, 10000))
-    #tripleta = (usuario, estado, identificador)
     cliente = ClienteChat(usuario, estado, host, port)
     #except:
-    #    print ('Algo salio mal, intente de nuevo')
-    #    sys.exit()
+        #print ('Algo salio mal, intente de nuevo')
+        #sys.exit()
 
 main()
--- src/Comportamiento.py
+++ src/Comportamiento.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import SalaDeChat
+class Eventos():
 
+    IDENTIFY = "IDENTIFY"
+    STATUS = "STATUS"
+    USERS = "USERS"
+    MESSAGE = "MESSAGE"
+    PUBLICMESSAGE = "PUBLICMESSAGE"
+    CREATEROOM = "CREATEROOM"
+    INVITE = "INVITE"
+    JOINROOM = "JOINROOM"
+    ROOMESSAGE = "ROOMESSAGE"
+    DISCONECT = "DISCONECT"
 
-class Comportamiento():
-
-    def ver_usuarios(self):
-
-    def envia_privado(self, usuario, mensaje):
-
-
-    def envia_publico(self, mensaje):
-
-        pass
-
-    def crea_sala(self, nombre):
-        pass
-
-    def invitar(self, sala, lista):
-        pass
-
-    def unirse(self, sala):
-        pass
-
-    def mensaje_sala(self, mensaje):
-        pass
-
-    def desconectar():
-        pass
+    def evento(self,msg):
+        try:
+            if msg == "IDENTIFY":
+                return self.IDENTIFY
+            if msg == "STATUS":
+                return self.STATUS
+            if msg == "MESSAGE":
+                return self.MESSAGE
+            if msg == "PUBLICMESSAGE":
+                return self.PUBLICMESSAGE
+            if msg == "CREATEROOM":
+                return self.CREATEROOM
+            if msg == "INVITE":
+                return self.INVITE
+            if msg == "JOINROOM":
+                return self.JOINROOM
+            if msg == "ROOMESSAGE":
+                return self.ROOMESSAGE
+            if msg == "DISCONECT":
+                return self.DISCONECT
+            else:
+                raise ValueError
+        except:
+            print("Evento no valido.")
